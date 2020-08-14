bd3f8640755a64155f54dd43944831f071b1ffd6
golovatenkop <p.golovatenko@elsevier.com>
2020-08-14 13:47:27 -0400

    another test case

--- example.bat
+++ example.bat
@@ -1,2 +1,3 @@
 python rnef2sbgn.py pathway.rnef pathway.sbgn "activity flow" 30
 python rnef2sbgn.py metabolic-reaction.rnef metabolic-reaction.sbgn "activity flow" 30
+python rnef2sbgn.py metabolic-reactions.rnef metabolic-reactions.sbgn "activity flow" 30
